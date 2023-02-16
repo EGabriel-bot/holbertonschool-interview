46e1e2aa340da1606edc1035bd315cf64286f125
Eiden <eidnegabriel07@gmail.com>
2023-02-16 19:40:27 -0400

    task 0

--- 0x09-utf8_validation/0-validate_utf8.py
+++ 0x09-utf8_validation/0-validate_utf8.py
@@ -2,52 +2,28 @@
 """Task 0"""
 
 
-def to_bits(bytes):
-    """converts a given byte into bits"""
-    for byte in bytes:
-        num = []
-        exp = 1 << NUMBER_OF_BITS_PER_BLOCK
-        while exp:
-            exp >>= 1
-            num.append(bool(byte & exp))
-        yield num
-
-
 def validUTF8(data):
     """determines if a given data set represents a valid UTF-8 encoding"""
-    bpc = 8
-    mno = 4
-    index = 0
-    while index < len(data):
-        number = data[index] & (2 ** 7)
-        number >>= (bpc - 1)
-        if number == 0:
-            index += 1
-            continue
-        number_of_ones = 0
-        while True:
-            number = data[index] & (2 ** (7 - number_of_ones))
-            number >>= (bpc - number_of_ones - 1)
-            if number == 1:
-                number_of_ones += 1
-            else:
-                break
-            if number_of_ones > mno:
+    b_1 = 1 << 7
+    b_2 = 1 << 6
+    nbytes = 0
+    if not data or len(data) == 0:
+        return True
+    for num in data:
+        bit = 1 << 7
+        if nbytes == 0:
+            while (bit & num):
+                nbytes += 1
+                bit = bit >> 1
+            if nbytes == 0:
+                continue
+            if nbytes == 1 or nbytes > 4:
                 return False
-        if number_of_ones == 1:
-            return False
-        index += 1
-        if index >= len(data) or index >= (index + number_of_ones - 1):
-            return False
-        for i in range(index, index + number_of_ones - 1):
-            number = data[i]
-
-            number >>= (bpc - 1)
-            if number != 1:
+        else:
+            if not (num & b_1 and not (num & b_2)):
                 return False
-            number >>= (bpc - 1)
-            if number != 0:
-                return False
-
-            index += 1
-    return True
+        nbytes -= 1
+    if nbytes:
+        return False
+    else:
+        return True
